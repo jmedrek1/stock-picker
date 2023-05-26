05c0a63c524355f94a0e82002858e17b6724697c
Jack Medrek <jackmedrek@Jacks-MacBook-Pro-2.local>
2023-05-25 20:23:15 -0400

    trying to set up API call

--- js/app.js
+++ js/app.js
@@ -3,6 +3,21 @@
 // temporary list of symbols as an example
 const tickerSymbols = ['AAPL', 'GOOG', 'TSLA', 'AMZN', 'MSFT', 'FB'];
 
+// fetch symbols
+const apiKey = 'D7JE7YVDE4QHC95D'; // don't lose this
+const exchange = 'NASDAQ'; // fetch stocks from NASDAQ exchange
+
+fetch(`https://www.alphavantage.co/query?function=LISTING_STATUS&exchange=${exchange}&apikey=${apiKey}`)
+    .then(response => response.json())
+    .then(data => {
+        // handle response data
+        console.log(data);
+    })
+    .catch(error => {
+        // handle errors
+        console.error(error);
+    });
+
 function getRandomStock() {
     const idx = Math.floor(Math.random() * tickerSymbols.length);
     return tickerSymbols[idx];
