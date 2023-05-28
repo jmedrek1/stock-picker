ba736e5b9f940231733db7117a1b9070ac41c337
Jack Medrek <jackmedrek@Jacks-MacBook-Pro-2.local>
2023-05-28 11:48:30 -0400

    loaded json data

--- js/app.js
+++ js/app.js
@@ -1,22 +1,21 @@
-// logic for choosing random symbol
+// ------- logic for choosing random symbol -----------
 
 // temporary list of symbols as an example
 const tickerSymbols = ['AAPL', 'GOOG', 'TSLA', 'AMZN', 'MSFT', 'FB'];
 
-// fetch symbols
-const apiKey = 'D7JE7YVDE4QHC95D'; // don't lose this
-const exchange = 'NASDAQ'; // fetch stocks from NASDAQ exchange
-
-fetch(`https://www.alphavantage.co/query?function=LISTING_STATUS&exchange=${exchange}&apikey=${apiKey}`)
+// fetch symbols from nasdaq.json - 5k stocks
+fetch('../assets/nasdaq.json')
     .then(response => response.json())
     .then(data => {
-        // Handle the response data
         console.log(data);
     })
     .catch(error => {
-        // Handle any errors
         console.error(error);
     });
+
+// fetch data from symbol
+const apiKey = 'D7JE7YVDE4QHC95D'; // don't lose this
+const exchange = 'NASDAQ'; // fetch stocks from NASDAQ exchange
 
 function getRandomStock() {
     const idx = Math.floor(Math.random() * tickerSymbols.length);
