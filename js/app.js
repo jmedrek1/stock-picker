4e439a87ad4c29561de48344c98f6d35e9e29fb8
Jack Medrek <jackmedrek@Jacks-MacBook-Pro-2.local>
2023-05-28 13:57:55 -0400

    picks random stock, shows ticker and security name

--- js/app.js
+++ js/app.js
@@ -1,32 +1,41 @@
 // ------- logic for choosing random symbol -----------
 
-// temporary list of symbols as an example
-const tickerSymbols = ['AAPL', 'GOOG', 'TSLA', 'AMZN', 'MSFT', 'FB'];
+let tickerSymbols = []; // Temporary list of symbols as an example
 
-// fetch symbols from nasdaq.json - 5k stocks
-fetch('../assets/nasdaq.json')
-    .then(response => response.json())
-    .then(data => {
-        console.log(data);
-    })
-    .catch(error => {
+function fetchSymbols() {
+    return fetch('../assets/nasdaq.json')
+        .then(response => response.json())
+        .then(data => {
+        tickerSymbols = data;
+        })
+        .catch(error => {
         console.error(error);
-    });
-
-// fetch data from symbol
-const apiKey = 'D7JE7YVDE4QHC95D'; // don't lose this
-const exchange = 'NASDAQ'; // fetch stocks from NASDAQ exchange
+        });
+}
 
 function getRandomStock() {
     const idx = Math.floor(Math.random() * tickerSymbols.length);
     return tickerSymbols[idx];
 }
 
-function updateTickerSymbol() {
+function updateStockInfo() {
+    const stock = getRandomStock();
+    updateTickerSymbol(stock);
+    updateStockName(stock);
+}
+
+function updateTickerSymbol(stock) {
     const tickerSymbolElement = document.getElementById('tickerSymbol');
-    const randomStock = getRandomStock();
-    tickerSymbolElement.innerText = randomStock;
+    tickerSymbolElement.innerText = stock['Symbol'];
+}
+
+function updateStockName(stock) {
+    const stockNameElement = document.getElementById('stockName');
+    stockNameElement.innerText = stock['Security Name'];
 }
 
 const pickButton = document.getElementById('pickButton');
-pickButton.addEventListener('click', updateTickerSymbol);
+pickButton.addEventListener('click', updateStockInfo);
+
+// Fetch the symbols when the page loads
+document.addEventListener('DOMContentLoaded', fetchSymbols);
